648a7c56abd2775af3c947961f5df0cbfe4d52cf
erickverleye <verleyee@msu.edu>
2022-07-05 21:55:36 -0500

    Initial Commit

---
+++ proxy_beta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  proxy_beta
+//
+//  Created by Erick Verleye on 7/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ proxy_beta/proxy_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  proxy_betaApp.swift
+//  proxy_beta
+//
+//  Created by Erick Verleye on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct proxy_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
